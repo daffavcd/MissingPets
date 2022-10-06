f2624ee4c8203bffb1b2583a09ea676fb51400b3
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-10-06 16:41:52 +0700

    front layout laporkan_kehilangan, add pub file_picker, datepicker

--- lib/pages/laporkan_kehilangan.dart
+++ lib/pages/laporkan_kehilangan.dart
@@ -1,5 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:async';
+import 'dart:io';
+
+const List<String> list = <String>['Kucing', 'Anjing', 'Burung', 'Lainnya'];
 
 class LaporkanKehilangan extends StatefulWidget {
   const LaporkanKehilangan({super.key});
@@ -10,10 +18,16 @@ class LaporkanKehilangan extends StatefulWidget {
 
 class _LaporkanKehilanganState extends State<LaporkanKehilangan> {
   final myController = TextEditingController();
+  final tanggalHilangController = TextEditingController();
+  final gambarController = TextEditingController();
+  final keteranganController = TextEditingController();
+
+  String dropdownValue = list.first;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: <Widget>[
           const SizedBox(
@@ -26,16 +40,23 @@ class _LaporkanKehilanganState extends State<LaporkanKehilangan> {
                   const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
             child: TextFormField(
-              obscureText: true,
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(fontWeight: FontWeight.normal),
+              ),
               enableSuggestions: false,
               autocorrect: false,
               controller: myController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: 'Enter your pin',
+                hintText: 'Nama Hewan',
+                filled: true,
+                labelText: 'Nama Hewan',
               ),
               validator: (value) {
                 if (value == null || value.isEmpty) {
@@ -46,8 +67,142 @@ class _LaporkanKehilanganState extends State<LaporkanKehilangan> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
+            child: ButtonTheme(
+                alignedDropdown: true,
+                child: DropdownButtonFormField<String>(
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  ),
+                  hint: const Text('Jenis Hewan'),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    labelText: 'Select City',
+                  ),
+                  value: dropdownValue,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      dropdownValue = value!;
+                    });
+                  },
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
+            child: TextFormField(
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(fontWeight: FontWeight.normal),
+              ),
+              readOnly: true,
+              controller: tanggalHilangController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: 'Pilih Tanggal',
+                filled: true,
+                labelText: 'Pilih Tanggal',
+                suffixIcon: IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.calendar_today),
+                ),
+              ),
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(
+                        2000), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime(2101));
+
+                if (pickedDate != null) {
+                  log(pickedDate
+                      .toString()); //pickedDate output format => 2021-03-10 00:00:00.000
+                  String formattedDate =
+                      DateFormat('yyyy-MM-dd').format(pickedDate);
+                  log(formattedDate); //formatted date output using intl package =>  2021-03-16
+                  //you can implement different kind of Date Format here according to your requirement
+
+                  setState(() {
+                    tanggalHilangController.text =
+                        formattedDate; //set output date to TextField value.
+                  });
+                } else {
+                  log("Date is not selected");
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
+            child: TextFormField(
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(fontWeight: FontWeight.normal),
+              ),
+              readOnly: true,
+              controller: gambarController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: 'Pilih Gambar Hewan',
+                filled: true,
+                labelText: 'Pilih Gambar Hewan',
+                suffixIcon: IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(Icons.image),
+                ),
+              ),
+              onTap: () async {
+                FilePickerResult? result =
+                    await FilePicker.platform.pickFiles(type: FileType.image);
+
+                if (result != null) {
+                  PlatformFile file = result.files.first;
+                  setState(() {
+                    gambarController.text = file.name;
+                  });
+
+                  // File file = File(result.files.single.path);
+                } else {
+                  // User canceled the picker
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
+            child: TextFormField(
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(fontWeight: FontWeight.normal),
+              ),
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              controller: keteranganController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Keterangan',
+                filled: true,
+                labelText: 'Keterangan',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 16.0),
             child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromRGBO(246, 157, 123, 1.0),
+                minimumSize: const Size.fromHeight(50), // NEW
+              ),
               onPressed: () {
                 if (myController.text == 'daffavcd') {
                   ScaffoldMessenger.of(context).showSnackBar(
